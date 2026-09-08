c13a543320b0a1bd019135d27c8089693ecff90a
sampannapandey46-oss <sampannapandey46@gmail.com>
2026-09-08 08:20:15 +0545

    Calculator ig

--- week 2/script.js
+++ week 2/script.js
@@ -1,16 +1,40 @@
 function addNumbers() {
 
-    // Get the values from the HTML inputs
     let number1 = document.getElementById("number1").value;
     let number2 = document.getElementById("number2").value;
 
-    // Convert the values into numbers
     number1 = Number(number1);
     number2 = Number(number2);
 
-    // Add the two numbers
     let result = number1 + number2;
 
-    // Display the result on the webpage
+    document.getElementById("result").innerHTML = result;
+}
+
+
+function subtractNumbers() {
+
+    let number1 = document.getElementById("number1").value;
+    let number2 = document.getElementById("number2").value;
+
+    number1 = Number(number1);
+    number2 = Number(number2);
+
+    let result = number1 - number2;
+
+    document.getElementById("result").innerHTML = result;
+}
+
+
+function multiplyNumbers() {
+
+    let number1 = document.getElementById("number1").value;
+    let number2 = document.getElementById("number2").value;
+
+    number1 = Number(number1);
+    number2 = Number(number2);
+
+    let result = number1 * number2;
+
     document.getElementById("result").innerHTML = result;
 }

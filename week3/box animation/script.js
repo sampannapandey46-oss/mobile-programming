1d64ae39a1a304875e0e6f015ad023df5563c43f
sampannapandey46-oss <sampannapandey46@gmail.com>
2026-09-20 06:48:28 +0545

    wow

---
+++ week3/box animation/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+```
+$("#start").click(function() {
+
+    $("#box").animate({
+        top: "250px"
+    }, 1000);
+
+    $("#box").animate({
+        left: "450px"
+    }, 1000);
+
+    $("#box").animate({
+        top: "0px"
+    }, 1000);
+
+    $("#box").animate({
+        left: "0px"
+    }, 1000);
+
+});
+```
+
+});
